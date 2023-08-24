d9270cb92691e1ff5525aba37545938fa1fe4377
JLMaldonado <141882305+JLMaldonado@users.noreply.github.com>
2023-08-24 16:07:09 -0600

    Resultados de la suma y descripción de variables

---
+++ formula.py
@@ -0,0 +1,4 @@
+n=input("ingresa tu numero entero: ")
+n=int(n)
+suma=(n*n+1)//2
+print(f"el resultado de tu suma es  {suma}")
---
+++ tiposdevariables.py
@@ -0,0 +1,3 @@
+numeros_enteros= int(1) 
+numeros_decimales=float(2.3)
+Trueofalse=bool(False/True)
